f702ce74792a08a71a267f0b200878c9a48172f1
Rasmus Göransson <rasmus@volumental.com>
2019-05-06 19:29:18 +0200

    Open serialport to servo controller from server

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,9 @@
 import express from 'express';
+import SerialPort from 'serialport';
+
+const path = '/dev/cu.usbserial-AL02L3L9';
+const serialport = new SerialPort(path, { baudRate: 57600 });
+
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
