1886196c124abee004f200c69bfded27f1ddb38d
Rasmus Göransson <rasmus@volumental.com>
2019-05-07 21:27:06 +0200

    Control robot from web browser

--- src/frontend/main.tsx
+++ src/frontend/main.tsx
@@ -4,12 +4,59 @@ import ReactDOM from "react-dom";
 import "./style.css";
 
 const App = () => {
-  const [c, increment] = useState(0);
-
+  const [lastCommand, updateLastCommand] = useState(' ');
+  const sendCommand = (command: string) => {
+    fetch(
+      '/',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          command,
+        }),
+      })
+      .then(console.log)
+      .catch(console.error);
+    updateLastCommand(command);
+  }
   return (
     <div className="App">
-      <h1>{c}</h1>
-      <button onClick={() => increment(c + 1)}>Press me</button>
+      <h1>Active command: {lastCommand}</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+            </td>
+            <td>
+              <button onClick={() => sendCommand('w')}>F</button>
+            </td>
+            <td>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button onClick={() => sendCommand('a')}>L</button>
+            </td>
+            <td>
+              <button onClick={() => sendCommand(' ')}>stop</button>
+            </td>
+            <td>
+              <button onClick={() => sendCommand('d')}>R</button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+            </td>
+            <td>
+              <button onClick={() => sendCommand('s')}>B</button>
+            </td>
+            <td>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 };
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
+import bodyParser from 'body-parser';
 import express from 'express';
 import SerialPort from 'serialPort';
 import path from 'path';
+import { isString } from 'util';
 
 const serialPortPath = '/dev/cu.usbserial-AL02L3L9';
 const serialPort = new SerialPort(serialPortPath, { baudRate: 57600 });
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Template configuration
 app.set('view engine', 'ejs');
@@ -20,9 +25,13 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-  console.log(`post body: '${req.body}'`);
-  serialPort.write(' ');
-  res.send('OK!');
+  console.log(`post body: '${JSON.stringify(req.body)}'`);
+  if (isString(req.body.command)) {
+    serialPort.write(req.body.command);
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(400);
+  }
 });
 
 app.listen(3000, () => console.log('Robot server app listening on http://localhost:3000'));
